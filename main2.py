639067cb1bf5b34b49c072d18234f6435ff2816c
IngenieroRicardo <138132387+IngenieroRicardo@users.noreply.github.com>
2023-06-29 08:22:02 -0600

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,8 @@
+print("Ingrese un numero:")
+numero = int(input())
+i = 1
+multiplicacion = 0
+while i < 11:
+  multiplicacion = numero * i
+  print(str(numero)+" x "+str(i)+" = "+str(multiplicacion))
+  i =  i + 1
